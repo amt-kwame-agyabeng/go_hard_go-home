5174e7eb22225ff0a3a9d3e122c60745378ab539
amt-kwame-agyabeng <kwameagyabeng63@gmail.com>
2025-05-07 13:44:36 +0000

    main module modified

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,33 +1,54 @@
 module "s3_storage" {
-    source = "./modules/01-Storage"
-    bucket_name = var.bucket_name
-  
+  source      = "./modules/01-Storage"
+  bucket_name = var.bucket_name
+
+}
+
+module "iam" {
+  source      = "./modules/04-IAM"
+  owner       = var.owner
+  environment = var.environment
+  bucket_name = var.bucket_name
+
 }
 
 
 module "networking" {
-    source = "./modules/02-Networking"
-    owner = var.owner
-    environment = var.environment 
-    region = var.region
-    vpc = var.vpc_name
-  
+  source           = "./modules/02-Networking"
+  owner            = var.owner
+  environment      = var.environment
+  region           = var.region
+  vpc              = var.vpc_name
+  vpc_cidr         = var.vpc_cidr
+  ngw_name         = var.ngw_name
+  igw_name         = var.igw_name
+  nat_eip_name     = var.nat_eip_name
+  private_rtb_name = var.private_rtb_name
+  public_rtb_name  = var.public_rtb_name
+  web_subnet_name  = var.web_subnet_name
+  app_subnet_name  = var.app_subnet_name
+  db_subnet_name   = var.db_subnet_name
+
+
 }
 
 module "security" {
-    source = "./modules/03-Security"
-    vpc_id = module.networking.vpc_id
-    owner = var.owner  
-    environment = var.environment
-    region = var.region
-    vpc = var.vpc_name
- 
+  source              = "./modules/03-Security"
+  vpc_id              = module.networking.vpc_id
+  owner               = var.owner
+  environment         = var.environment
+  region              = var.region
+  vpc                 = var.vpc_name
+  jump_server_sg_name = var.jump_server_sg_name
+  alb_sg_name         = var.alb_sg_name
+  rds_sg_name         = var.rds_sg_name
+  ecs_sg_name         = var.ecs_sg_name
+  ssh_port            = var.ssh_port
+  http_port           = var.http_port
+  https_port          = var.https_port
+  mysql_port          = var.mysql_port
+  waf_acl_name        = var.waf_acl_name
+
+
 }
 
-module "iam" {
-    source = "./modules/04-IAM"
-    owner = var.owner
-    environment = var.environment
-    bucket_name = var.bucket_name
-  
-}
